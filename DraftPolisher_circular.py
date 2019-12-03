54a21dffc00314df99e90b2d956c8680c7b77a9c
BioRB <quasibiologo@gmail.com>
2019-12-03 11:28:50 +0100

    aa

--- DraftPolisher_circular.py
+++ DraftPolisher_circular.py
@@ -2682,4 +2682,4 @@ elif answer == "no":
     print("all the process files will be discarded exept the consensus.fa file!")
     os.system("find . -type f -not -name 'consensus.fa' -delete")
 else:
-    print("Please enter yes or no.")
+    print("Please enter yes or no.")a
